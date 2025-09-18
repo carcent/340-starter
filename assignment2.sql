1787977e7df08961912fd91a27294522b97e2a06
carcent <carli.centurion@gmail.com>
2025-09-17 22:13:05 -0300

    add folders add js files add inventory created sql

---
+++ assignment2.sql
@@ -0,0 +1,39 @@
+--1. insert new record 
+INSERT INTO public.account(
+	account_firstname, 
+	account_lastname, 
+	account_email, 
+	account_password
+)
+VALUES (
+	'Tony',
+	'Stark',
+	'tony@starkent.com',
+	'Iam1ronM@n'
+);
+
+--2 modify record to Admin
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com'
+
+--3 Delete Tony Stark
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+
+--4 Modify GM Hummer
+UPDATE inventory
+SET  inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_model = 'Hummer';
+
+--5- Select inv_make and inv_model and classification_name
+SELECT i.inv_make, i.inv_model, c.classification_name
+fROM inventory i
+INNER JOIN classification c
+	ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+--6- update records in the inventory /vehicles
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/','/images/vehicles/');
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +27,7 @@ app.set("layout", "./layouts/layout") //not at views root
 app.use(static)
 
 // Index Route
-app.get("/", function(req, res){
-  res.render("index", {title:"Home"})
-})
+app.get("/", baseController.buildHome)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -35,9 +35,13 @@ app.get("/", function(req, res){
 const port = process.env.PORT
 const host = process.env.HOST
 
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
